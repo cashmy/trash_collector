ce89c8358304862d34bad7788eacee355ff547fb
cashmy <cmyers880@gmail.com>
2021-04-09 11:26:03 -0500

    Added addressType as a primary key field

--- trash_collector/customeraddresses/models.py
+++ trash_collector/customeraddresses/models.py
@@ -7,3 +7,5 @@ from django.db import models
 class CustomerAddress(models.Model):
     customer_id = models.ForeignKey('customers.id', on_delete=models.SET_NULL)
     address_id = models.ForeignKey('address.id', on_delete=models.CASCADE)
+    address_type = models.CharField(max_length=1, primary_key=True)
+
