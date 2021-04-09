aa7909f4033e6b9193ff51bf0de0ac166a75f6a8
cashmy <cmyers880@gmail.com>
2021-04-09 16:13:11 -0500

    Added __str__ function to CustomerAddress. Added models Employee, CustomerAddress, Address to Admin module

--- trash_collector/customers_addresses/models.py
+++ trash_collector/customers_addresses/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 class CustomerAddress(models.Model):
-    customer_id = models.ForeignKey('customers.id', on_delete=models.SET_NULL)
-    address_id = models.ForeignKey('address.id', on_delete=models.CASCADE)
+    customer_id = models.ForeignKey('customers.Customer', on_delete=models.CASCADE)
+    address_id = models.ForeignKey('addresses.Address', on_delete=models.CASCADE)
     address_type = models.CharField(max_length=1, primary_key=True)
+
+    def __str__(self):
+        if self.address_type == "P":
+            address_string = 'Pickup for ' + str(self.customer_id)
+        else:
+            address_string = 'Billing for ' + str(self.customer_id)
+        return address_string
